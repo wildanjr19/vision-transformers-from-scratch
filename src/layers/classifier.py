e4f8c62e244accc052c367290e0b4556852d15b8
wildanjr19 <wildansekolah11@gmail.com>
2026-01-25 14:59:32 +0700

    classifier head

---
+++ src/layers/classifier.py
@@ -0,0 +1,39 @@
+"""
+MLP Head atau classifier untuk klasifikasi
+Paper asli ViT menggunakan 1 layer mlp sebagai classifier (single linear layer)
+"""
+
+import torch
+import torch.nn as nn
+from config.CONFIG import EMBED_DIM, NUM_CLASSES
+
+class VITClassifier(nn.Module):
+    """
+    Args:
+        - embed_dim : dimensi embedding input (output dari transformer encoder)
+        - num_classes : kelas untuk klasifikasi
+        - hidden features : None karena satu layer, tapi bisa diisi (original paper tidak ada)
+        - drop : dropout
+    """
+    def __init__(self, embed_dim = EMBED_DIM, num_classes = NUM_CLASSES, hidden_features = None, drop = 0):
+        super().__init__()
+        self.num_classes = num_classes
+        self.embed_dim = embed_dim
+
+        # single linear layer
+        if hidden_features is None:
+            self.head = nn.Sequential(
+                nn.Dropout(drop),
+                nn.Linear(embed_dim, num_classes)
+            )
+        # jika hidden_features diisi
+        else:
+            self.head = nn.Sequential(
+                nn.Linear(embed_dim, hidden_features),
+                nn.Tanh(),
+                nn.Dropout(drop),
+                nn.Linear(hidden_features, num_classes)
+            )
+
+    def forward(self, x):
+        return self.head(x)
