1cf0d01932aa4fa451d3c6050d25329c4845256f
wildanjr19 <wildansekolah11@gmail.com>
2026-02-01 21:49:22 +0700

    config for mnist train

--- config/CONFIG.py
+++ config/CONFIG.py
@@ -1,13 +1,18 @@
 """
 Konfigurasi untuk ViT
 """
+import torch
 
 IMAGE_SIZE = 28
-PATCH_SIZE = 7
+PATCH_SIZE = 4
 NUM_CHANNELS = 1
 EMBED_DIM = 64
-ATTENTION_HEADS = 8
+ATTENTION_HEADS = 4
 MLP_RATIO = 4
 HIDDEN_DIM = EMBED_DIM * MLP_RATIO
 NUM_CLASSES = 10
-DEPTH = 12
+DEPTH = 3
+BATCH_SIZE = 64
+EPOCHS = 10
+LEARNING_RATE = 0.001
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
